dd1efbf7831d95c52a95b169232c4e8897bbf25c
carol <carolinamartinsfreitas@gmail.com>
2020-10-02 17:33:00 -0300

    Adicionando a View de detalhe do Sobre

--- backend/content/urls.py
+++ backend/content/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [ 
     path('', views.sobre_view, name='sobre_view'),
+    path('sobre/', views.sobre_detalhe, name='sobre_detalhe')
 ] 
   
--- backend/content/views.py
+++ backend/content/views.py
@@ -1,20 +1,11 @@
-from django.http import HttpResponse 
-from django.shortcuts import render, redirect 
-from .forms import *
-  
- 
-def sobre_view(request): 
-  
-    if request.method == 'POST': 
-        form = SobreForm(request.POST, request.FILES) 
-  
-        if form.is_valid(): 
-            form.save() 
-            return redirect('success') 
-    else: 
-        form = SobreForm() 
-    return render(request, 'sobre.html', {'form' : form}) 
-  
-  
-def success(request): 
-    return HttpResponse('successfully uploaded') 
+from django.shortcuts import render
+from .models import Sobre
+from django.shortcuts import render, get_object_or_404
+
+def sobre_view(request):
+    sobre = Sobre.objects.all()
+    return render(request, 'content/sobre.html', {'sobre':sobre})
+
+def sobre_detalhe(request):
+    sobre = get_object_or_404(Sobre)
+    return render(request, 'content/sobre_detalhe.html', {'sobre': sobre})
